a28efcf746df935cbe731183882d77eec4da1cd6
Rubén López <lopezrbn@gmail.com>
2025-09-17 10:11:19 +0200

    Refactor model loading to dynamically find the best model based on the current year and month

--- pipelines/d_inference/API.py
+++ pipelines/d_inference/API.py
@@ -9,10 +9,31 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import pandas as pd
+from datetime import datetime
 
+
+CURRENT_YEAR = datetime.now().year
+CURRENT_MONTH = datetime.now().month
+
+
+def find_best_model(current_year=CURRENT_YEAR, current_month=CURRENT_MONTH):
+    model_path = paths.MODELS_DIR / f"{current_year}{str(current_month).zfill(2)}_best_model.pkl"
+    if model_path.exists():
+        print(f"Best model found for {current_year}{str(current_month).zfill(2)}")
+        return model_path
+    else:
+        print(f"No model found for {current_year}{str(current_month).zfill(2)}. Checking previous months...")
+        current_month -= 1
+        if current_month == 0:
+            current_month = 12
+            current_year -= 1
+            if current_year < 2025:
+                raise ValueError("No model available before 2025.")
+        return find_best_model(current_year, current_month)
 
 # Load the model
-model = joblib.load(paths.BEST_MODEL_FILE)
+model_path = find_best_model()
+model = joblib.load(model_path)
 
 # Define the entrypoint for the API
 class Car(BaseModel):
